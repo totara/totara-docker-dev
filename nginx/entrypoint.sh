4f7fae66b77207a05ce2514188d1826c83eacf92
Fabian Derschatta <fabian.derschatta@totaralearning.com>
2017-11-23 12:57:00 +1300

    Optimise permissions

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,54 +4,33 @@ cd ${REMOTE_DATA}
 
 # version 9
 
-mkdir -p ver9.mssql
-mkdir -p ver9.mysql
-mkdir -p ver9.pgsql
+mkdir -p ver9.mssql ver9.mysql ver9.pgsql
+chown -R www-data:www-data ver9.mssql ver9.mysql ver9.pgsql
+chmod g+s ver9.mssql ver9.mysql ver9.pgsql
 
-chown -R www-data:www-data ver9.mssql
-chown -R www-data:www-data ver9.mysql
-chown -R www-data:www-data ver9.pgsql
-
-mkdir -p ver9.mssql.phpunit
-mkdir -p ver9.mysql.phpunit
-mkdir -p ver9.pgsql.phpunit
-
-mkdir -p ver9.pgsql.behat
+mkdir -p ver9.mssql.phpunit ver9.mysql.phpunit ver9.pgsql.phpunit ver9.pgsql.behat
 chown -R www-data:www-data ver9.pgsql.behat
+chmod g+s ver9.pgsql.behat
 
 # version 10
 
-mkdir -p ver10.mssql
-mkdir -p ver10.mysql
-mkdir -p ver10.pgsql
+mkdir -p ver10.mssql ver10.mysql ver10.pgsql
+chown -R www-data:www-data ver10.mssql ver10.mysql ver10.pgsql
+chmod g+s ver10.mssql ver10.mysql ver10.pgsql
 
-chown -R www-data:www-data ver10.mssql
-chown -R www-data:www-data ver10.mysql
-chown -R www-data:www-data ver10.pgsql
-
-mkdir -p ver10.mssql.phpunit
-mkdir -p ver10.mysql.phpunit
-mkdir -p ver10.pgsql.phpunit
-
-mkdir -p ver10.pgsql.behat
+mkdir -p ver10.mssql.phpunit ver10.mysql.phpunit ver10.pgsql.phpunit ver10.pgsql.behat
 chown -R www-data:www-data ver10.pgsql.behat
+chmod g+s ver10.mssql ver10.pgsql.behat
 
 # version 11
 
-mkdir -p ver11.mssql
-mkdir -p ver11.mysql
-mkdir -p ver11.pgsql
+mkdir -p ver11.mssql ver11.mysql ver11.pgsql
+chown -R www-data:www-data ver11.mssql ver11.mysql ver11.pgsql
+chmod g+s ver11.mssql ver11.mysql ver11.pgsql
 
-chown -R www-data:www-data ver11.mssql
-chown -R www-data:www-data ver11.mysql
-chown -R www-data:www-data ver11.pgsql
-
-mkdir -p ver11.mssql.phpunit
-mkdir -p ver11.mysql.phpunit
-mkdir -p ver11.pgsql.phpunit
-
-mkdir -p ver11.pgsql.behat
+mkdir -p ver11.mssql.phpunit ver11.mysql.phpunit ver11.pgsql.phpunit ver11.pgsql.behat
 chown -R www-data:www-data ver11.pgsql.behat
+chmod g+s ver11.mssql ver11.pgsql.behat
 
 # fire up nginx
 nginx -g 'daemon off;'
