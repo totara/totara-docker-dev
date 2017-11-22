317a6c45b578166b67c914df0a7832f8820d4b1c
Fabian Derschatta <fabian.derschatta@totaralearning.com>
2017-11-23 11:33:08 +1300

    Make sure data directories are created and have the correct permissions

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+cd ${REMOTE_DATA}
+
+# version 9
+
+mkdir -p ver9.mssql
+mkdir -p ver9.mysql
+mkdir -p ver9.pgsql
+
+chown -R www-data:www-data ver9.mssql
+chown -R www-data:www-data ver9.mysql
+chown -R www-data:www-data ver9.pgsql
+
+mkdir -p ver9.mssql.phpunit
+mkdir -p ver9.mysql.phpunit
+mkdir -p ver9.pgsql.phpunit
+
+mkdir -p ver9.pgsql.behat
+chown -R www-data:www-data ver9.pgsql.behat
+
+# version 10
+
+mkdir -p ver10.mssql
+mkdir -p ver10.mysql
+mkdir -p ver10.pgsql
+
+chown -R www-data:www-data ver10.mssql
+chown -R www-data:www-data ver10.mysql
+chown -R www-data:www-data ver10.pgsql
+
+mkdir -p ver10.mssql.phpunit
+mkdir -p ver10.mysql.phpunit
+mkdir -p ver10.pgsql.phpunit
+
+mkdir -p ver10.pgsql.behat
+chown -R www-data:www-data ver10.pgsql.behat
+
+# version 11
+
+mkdir -p ver11.mssql
+mkdir -p ver11.mysql
+mkdir -p ver11.pgsql
+
+chown -R www-data:www-data ver11.mssql
+chown -R www-data:www-data ver11.mysql
+chown -R www-data:www-data ver11.pgsql
+
+mkdir -p ver11.mssql.phpunit
+mkdir -p ver11.mysql.phpunit
+mkdir -p ver11.pgsql.phpunit
+
+mkdir -p ver11.pgsql.behat
+chown -R www-data:www-data ver11.pgsql.behat
+
+
