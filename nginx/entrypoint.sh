6dbe95627d6c60dfb6f612e7be716d62093196cc
Fabian Derschatta <fabian.derschatta@totaralearning.com>
2018-01-29 13:58:23 +1300

    Define versions in array and loop through it to setup data folders

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -6,55 +6,20 @@ cd $REMOTE_DATA
 
 echo "Creating data folders..."
 
-# version 2.7
+versions=("22" "24" "25" "26" "27" "29" "9" "10" "11")
 
-mkdir -p ver27.mssql ver27.mysql ver27.pgsql
-chown -R www-data:www-data ver27.mssql ver27.mysql ver27.pgsql
-chmod g+s ver27.mssql ver27.mysql ver27.pgsql
+for i in "${versions[@]}"
+do
+   :
+   echo "Creating folders for version $i"
+   mkdir -p ver$i.mssql ver$i.mysql ver$i.pgsql
+   chown -R www-data:www-data ver$i.mssql ver$i.mysql ver$i.pgsql
+   chmod g+s ver$i.mssql ver$i.mysql ver$i.pgsql
 
-mkdir -p ver27.mssql.phpunit ver27.mysql.phpunit ver27.pgsql.phpunit ver27.pgsql.behat
-chown -R www-data:www-data ver27.pgsql.behat
-chmod g+s ver27.pgsql.behat
-
-# version 2.9
-
-mkdir -p ver29.mssql ver29.mysql ver29.pgsql
-chown -R www-data:www-data ver29.mssql ver29.mysql ver29.pgsql
-chmod g+s ver29.mssql ver29.mysql ver29.pgsql
-
-mkdir -p ver29.mssql.phpunit ver29.mysql.phpunit ver29.pgsql.phpunit ver29.pgsql.behat
-chown -R www-data:www-data ver29.pgsql.behat
-chmod g+s ver29.pgsql.behat
-
-# version 9
-
-mkdir -p ver9.mssql ver9.mysql ver9.pgsql
-chown -R www-data:www-data ver9.mssql ver9.mysql ver9.pgsql
-chmod g+s ver9.mssql ver9.mysql ver9.pgsql
-
-mkdir -p ver9.mssql.phpunit ver9.mysql.phpunit ver9.pgsql.phpunit ver9.pgsql.behat
-chown -R www-data:www-data ver9.pgsql.behat
-chmod g+s ver9.pgsql.behat
-
-# version 10
-
-mkdir -p ver10.mssql ver10.mysql ver10.pgsql
-chown -R www-data:www-data ver10.mssql ver10.mysql ver10.pgsql
-chmod g+s ver10.mssql ver10.mysql ver10.pgsql
-
-mkdir -p ver10.mssql.phpunit ver10.mysql.phpunit ver10.pgsql.phpunit ver10.pgsql.behat
-chown -R www-data:www-data ver10.pgsql.behat
-chmod g+s ver10.mssql ver10.pgsql.behat
-
-# version 11
-
-mkdir -p ver11.mssql ver11.mysql ver11.pgsql
-chown -R www-data:www-data ver11.mssql ver11.mysql ver11.pgsql
-chmod g+s ver11.mssql ver11.mysql ver11.pgsql
-
-mkdir -p ver11.mssql.phpunit ver11.mysql.phpunit ver11.pgsql.phpunit ver11.pgsql.behat
-chown -R www-data:www-data ver11.pgsql.behat
-chmod g+s ver11.mssql ver11.pgsql.behat
+   mkdir -p ver$i.mssql.phpunit ver$i.mysql.phpunit ver$i.pgsql.phpunit ver$i.pgsql.behat
+   chown -R www-data:www-data ver$i.pgsql.behat
+   chmod g+s ver$i.pgsql.behat
+done
 
 echo "done"
 
