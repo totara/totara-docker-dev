e854e78fc863b8beadf25888c50770329f2259a7
Fabian Derschatta <fabian@derschatta.de>
2018-07-12 14:25:50 +1200

    Add ssl support to nginx vhosts

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -23,5 +23,20 @@ done
 
 echo "done"
 
+# if there's no ssl certificate yet create it
+if [ ! -f "/etc/nginx/ssl/domain.crt" ]
+then
+    mkdir -p /etc/nginx/ssl
+    openssl req \
+        -new \
+        -newkey rsa:4096 \
+        -days 3650 \
+        -nodes \
+        -x509 \
+        -subj "/C=US/ST=CA/L=SF/O=Docker-demo/CN=totara" \
+        -keyout /etc/nginx/ssl/domain.key \
+        -out /etc/nginx/ssl/domain.crt
+fi
+
 # fire up nginx
 nginx -g 'daemon off;'
