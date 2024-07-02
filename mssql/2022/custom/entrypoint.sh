eacb07c375b091287245e3a7760887a5c2afc014
Cody Finegan <cody.finegan@totara.com>
2024-07-02 13:21:43 +1200

    Support for local self-signed certificates

--- mssql/2022/custom/entrypoint.sh
+++ mssql/2022/custom/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-# start SQL Server, start the script to create the DB and import the data, start the app
-/opt/mssql/bin/sqlservr & /usr/config/setup.sh & tail -f /dev/null
+# Start the script to create the DB and import the data, start the app
+/usr/config/setup.sh &
+
+# Start the server
+/opt/mssql/bin/sqlservr
