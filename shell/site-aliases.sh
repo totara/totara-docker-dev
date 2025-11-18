45d5564798388d54200eea7e6a3c1ed33cec5d36
Mark Metcalfe <mark.metcalfe@totara.com>
2025-11-19 11:39:56 +1300

    fix: Remove vendor directories when installing a T20+ site

--- shell/site-aliases.sh
+++ shell/site-aliases.sh
@@ -4,9 +4,17 @@
 # Perform a fresh install of Totara
 install() {
   site_root || return 1
+
+  local version=$(totara_version major)
+  if [[ $version -ge 20 ]]; then
+    # From Totara 20 onwards, composer packages aren't included in the repo,
+    # so we need to remove any existing vendor directories to ensure a clean install.
+    rm -rf vendor server/vendor
+  fi
+
   if [[ -z "$1" ]]; then
     # Make the site name the version of the Totara site
-    local site_name="Totara $(totara_version major) development site"
+    local site_name="Totara $version development site"
     run_totara_cmd php admin/cli/install_database.php --adminpass=admin --adminemail=admin@example.com --agree-license --shortname=$site_name --fullname=$site_name
   else
     run_totara_cmd php admin/cli/install_database.php $@
